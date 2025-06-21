48971489e303878cd4cacf7e0316f43474975797
Itai Schvartzbard <itai@practitest.com>
2025-06-21 19:06:42 +0300

    App css and main page with Wordle component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Wordle from "./components/Wordle";
+import {TimerBox} from "./playground";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Wordle />
+        <TimerBox startSeconds={10} />
     </div>
   );
 }
